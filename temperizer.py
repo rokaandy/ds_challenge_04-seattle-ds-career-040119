069c288ba8d81ac5b32679d801f5ff36f758208e
Andy Luc <rokaandy@gmail.com>
2019-04-04 10:32:38 -0700

    Modify temperizer.py and test_temperizer.ipynb

--- temperizer.py
+++ temperizer.py
@@ -9,4 +9,29 @@ def convert_f_to_c(temperature_f):
 
 def convert_c_to_f(temperature_c):
     """Convert Celsius to Fahrenheit."""
-    pass
+    temperature_f = (temperature_c * (9/5) + 32)
+    return temperature_f
+
+
+def convert_c_to_k(temperature_c):
+    """Convert Celsius to Kelvin."""
+    temperature_k = (temperature_c + 273.15)
+    return temperature_k
+
+
+def convert_f_to_k(temperature_f):
+    """Convert Fahrenheit to Kelvin."""
+    temperature_k = ((temperature_f - 32)*(5/9) + 273.15)
+    return temperature_k
+
+
+def convert_k_to_c(temperature_k):
+    """Convert Kelvin to Celsius."""
+    temperature_c = (temperature_k - 273.15)
+    return temperature_c
+
+
+def convert_k_to_f(temperature_k):
+    """Convert Kelvin to Fahrenheit."""
+    temperature_f = ((temperature_k - 273.15)*(9/5) + 32)
+    return temperature_f
